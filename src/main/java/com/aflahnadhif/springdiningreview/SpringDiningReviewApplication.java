5e59888267c8c22c0ac3a620ad3140188e37ca90
Aflah Nadhif <aflahnadhif@gmail.com>
2023-04-28 12:46:16 +0700

    Project initializing: Installed Spring Boot Web, JPA, H2, Lombok

---
+++ src/main/java/com/aflahnadhif/springdiningreview/SpringDiningReviewApplication.java
@@ -0,0 +1,13 @@
+package com.aflahnadhif.springdiningreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDiningReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDiningReviewApplication.class, args);
+	}
+
+}
